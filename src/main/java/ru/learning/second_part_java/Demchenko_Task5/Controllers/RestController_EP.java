8f446dd850cb2104f5631bf69b56e502d31f476e
Демченко Николай Алексеевич <nademchenko@dev.vtb.ru>
2024-07-26 10:29:31 +0300

    Коммит_4_Готовый_проект_добавил_обработку_500_статуса

--- src/main/java/ru/learning/second_part_java/Demchenko_Task5/Controllers/RestController_EP.java
+++ src/main/java/ru/learning/second_part_java/Demchenko_Task5/Controllers/RestController_EP.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.learning.second_part_java.Demchenko_Task5.Model.Model_EP;
 import ru.learning.second_part_java.Demchenko_Task5.Services.Service_EP;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 
 @RestController
 @RequestMapping(value="/")
@@ -22,8 +25,15 @@ public class RestController_EP {
 
     @PostMapping(value = "corporate-settlement-instance/create")
     public ResponseEntity<String> createProdReg(@RequestBody Model_EP request) {
-        retValue = service_EP.create_EP(request);
-        System.out.println("retValue "+retValue);
+        try {
+            retValue = service_EP.create_EP(request);
+            System.out.println("retValue " + retValue);
+        }catch (Exception e)
+            {
+                StringWriter err_msg=new StringWriter();
+                e.printStackTrace(new PrintWriter(err_msg));
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()+"\n"+err_msg);
+            }
 
         statusRequest = Integer.valueOf(retValue.substring(0, 3));
         retValue = retValue.substring(3);
--- src/main/java/ru/learning/second_part_java/Demchenko_Task5/Controllers/RestController_ProdReg.java
+++ src/main/java/ru/learning/second_part_java/Demchenko_Task5/Controllers/RestController_ProdReg.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.learning.second_part_java.Demchenko_Task5.Services.Service_ProdReg;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @RestController
 @RequestMapping(value="/")
 public class RestController_ProdReg {
@@ -21,7 +24,16 @@ public class RestController_ProdReg {
 
     @PostMapping(value = "corporate-settlement-account/create")
     public ResponseEntity<String> createProdReg(@RequestBody Model_ProdReg request) {
-        retValue = service_prodReg.create_ProdReg(request);
+        try{
+            retValue = service_prodReg.create_ProdReg(request);
+            }
+        catch (Exception e)
+        {
+            StringWriter err_msg=new StringWriter();
+            e.printStackTrace(new PrintWriter(err_msg));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()+"\n"+err_msg);
+        }
+
         statusRequest = Integer.valueOf(retValue.substring(0, 3));
         retValue = retValue.substring(3);
 
